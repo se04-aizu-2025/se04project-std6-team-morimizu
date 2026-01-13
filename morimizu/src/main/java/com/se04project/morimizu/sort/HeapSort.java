e5d4a4722078ec0075a944039d63512a682ca0a7
mori_shotaro <mori_shotaro@plise.co.jp>
2026-01-13 14:32:47 +0900

    HeapSort, RadixSort追加

--- morimizu/src/main/java/com/se04project/morimizu/sort/HeapSort.java
+++ morimizu/src/main/java/com/se04project/morimizu/sort/HeapSort.java
@@ -1,5 +1,48 @@
 package com.se04project.morimizu.sort;
 
 public class HeapSort {
+    static int length;
 
+    public static void heapSort(int[] arr) {
+        length = arr.length;
+        if (length < 1)
+            return;
+
+        buildMaxHeap(arr);
+        while (length > 0) {
+            swap(arr, 0, length - 1);
+            length--;
+            adjustHeap(arr, 0);
+        }
+    }
+
+    public static void buildMaxHeap(int[] arr) {
+        for (int i = (length / 2 - 1); i >= 0; i--) {
+            adjustHeap(arr, i);
+        }
+    }
+
+    public static void adjustHeap(int[] arr, int i) {
+        int maxIndex = i;
+
+        int left = i * 2 + 1;
+        int right = i * 2 + 2;
+        if (left < length && arr[left] > arr[maxIndex]) {
+            maxIndex = left;
+        }
+        if (right < length && arr[right] > arr[maxIndex]) {
+            maxIndex = right;
+        }
+        if (maxIndex != i) {
+            swap(arr, maxIndex, i);
+            adjustHeap(arr, maxIndex);
+        }
+    }
+
+    public static void swap(int[] arr, int from, int to) {
+        int temp = 0;
+        temp = arr[from];
+        arr[from] = arr[to];
+        arr[to] = temp;
+    }
 }
--- morimizu/src/main/java/com/se04project/morimizu/sort/RadixSort.java
+++ morimizu/src/main/java/com/se04project/morimizu/sort/RadixSort.java
@@ -1,5 +1,42 @@
 package com.se04project.morimizu.sort;
 
-public class RadixSort {
+import java.util.ArrayList;
 
+public class RadixSort {
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        int maxDigit = 0;
+        while (max != 0) {
+            max /= 10;
+            maxDigit++;
+        }
+        int mod = 10;
+        int div = 1;
+        ArrayList<ArrayList<Integer>> bucketList = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < 10; i++) {
+            bucketList.add(new ArrayList<>());
+        }
+        for (int i = 0; i < maxDigit; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                int num = (arr[j] % mod) / div;
+                bucketList.get(num).add(arr[j]);
+            }
+            int index = 0;
+            for (int j = 0; j < bucketList.size(); j++) {
+                ArrayList<Integer> bucket = bucketList.get(j);
+                for (Integer val : bucket) {
+                    arr[index++] = val;
+                }
+                bucket.clear();
+            }
+            mod *= 10;
+            div *= 10;
+        }
+    }
 }
