031bd808306c982dd7581dadcb9d4a118c5b5fe8
s1310087 <s1310087@u-aizu.ac.jp>
2026-01-27 14:28:38 +0900

    エディターデザインとレイアウトの調整

--- morimizu/src/main/java/com/se04project/morimizu/MainController.java
+++ morimizu/src/main/java/com/se04project/morimizu/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     @GetMapping("/home")
-    public String display() {
+    public String displayHome() {
         return "index";
     }
 }
